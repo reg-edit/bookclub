7f3b343c353602b3036ff55140c41e0d90425fd1
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-08 12:14:20 +0100

    Add signup form and CSS.

--- signup.php
+++ signup.php
@@ -3,6 +3,20 @@
 ?>
 
 
+<section class="signup-form">
+    <h2>Sign Up</h2>
+    <div class="signup-form-form">
+        <form action="signup.inc.php" method="post">
+            <input type="text" name="name" placeholder="Full name...">
+            <input type="text" name="email" placeholder="Email...">
+            <input type="text" name="uid" placeholder="Username...">
+            <input type="password" name="pwd" placeholder="Password...">
+            <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+            <button type="submit" name="submit">Sign Up</button>
+
+        </form>
+    </div>
+</section>
 
 
 <?php
