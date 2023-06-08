45fada5add11bbebe1a733e105dfd56291ab8c45
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-08 15:57:16 +0100

    Add basic login page and update style.

--- login.php
+++ login.php
@@ -3,6 +3,17 @@
 ?>
 
 
+<section class="signup-form">
+    <h2>Login</h2>
+    <div class="signup-form-form">
+        <form action="login.inc.php" method="post">
+            <input type="text" name="name" placeholder="Username/Email...">
+            <input type="password" name="pwd" placeholder="Password...">
+            <button type="submit" name="submit">Login</button>
+
+        </form>
+    </div>
+</section>
 
 
 <?php
