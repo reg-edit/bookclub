dd2f4b0efc046af861c05ce0d757215bc5845d97
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-08 11:43:22 +0100

    Changed to using header.php instead of repeating HTML

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Sexy Book Club</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-
-    <nav>
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="discover.php">ABOUT US</a></li>
-                <li><a href="signup.php">SIGN UP</a></li>
-                <li><a href="login.php">LOGIN</a></li>
-            </ul>
-        </div>
-    </nav>
-    
-    <div class="wrapper">
+<?php
+    include_once 'header.php'
+?>
 
         <section class="index-intro">
             <h1>Welcome to The Sexy Book Club</h1>
--- signup.php
+++ signup.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Sexy Book Club</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-
-    <nav>
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="discover.php">ABOUT US</a></li>
-                <li><a href="signup.php">SIGN UP</a></li>
-                <li><a href="login.php">LOGIN</a></li>
-            </ul>
-        </div>
-    </nav>
-
-
-</body>
-</html>
-<script src="js/script.js"></script> 
+<?php
+    include_once 'header.php'
+?>
