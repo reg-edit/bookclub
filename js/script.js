e1f52df2c4f1b9f3b39123ddc6261b3838d13654
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-08 17:59:07 +0100

    Add functions for handling sign ups.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 let navBar = document.querySelectorAll("nav ul li a")
 let currentUrl = window.location.href; 
 
-if (currentUrl.endsWith("index.php")) {
+if (currentUrl.includes("index.php")) {
     navBar[0].style.color = "rgb(119, 119, 219)" 
-} else if (currentUrl.endsWith("discover.php")) {
+} else if (currentUrl.includes("discover.php")) {
     navBar[1].style.color = "rgb(119, 119, 219)" 
-} else if (currentUrl.endsWith("signup.php")) {
+} else if (currentUrl.includes("signup.php")) {
     navBar[2].style.color = "rgb(119, 119, 219)" 
-} else if (currentUrl.endsWith("login.php")) {
+} else if (currentUrl.includes("login.php")) {
     navBar[3].style.color = "rgb(119, 119, 219)" 
 }
 
