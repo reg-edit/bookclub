04d496acd11aec5653ae3e49b00161ae7a53b7bf
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-08 17:57:41 +0100

    Fix bug where passwords werent registering as filled.

--- signup.php
+++ signup.php
@@ -6,12 +6,12 @@
 <section class="signup-form">
     <h2>Sign Up</h2>
     <div class="signup-form-form">
-        <form action="signup.inc.php" method="post">
+        <form action="includes/signup.inc.php" method="post">
             <input type="text" name="name" placeholder="Full name...">
             <input type="text" name="email" placeholder="Email...">
             <input type="text" name="uid" placeholder="Username...">
             <input type="password" name="pwd" placeholder="Password...">
-            <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+            <input type="password" name="pwdRepeat" placeholder="Repeat password...">
             <button type="submit" name="submit">Sign Up</button>
 
         </form>
