04caa5b6d0b797df72d7bd6a269ef56f520ea7ed
Reg-Edit <redwardsbusiness@gmail.com>
2023-06-09 16:48:08 +0100

    Add error display box for error handling

--- login.php
+++ login.php
@@ -6,13 +6,27 @@
 <section class="signup-form">
     <h2>Login</h2>
     <div class="signup-form-form">
-        <form action="login.inc.php" method="post">
-            <input type="text" name="name" placeholder="Username/Email...">
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username/Email...">
             <input type="password" name="pwd" placeholder="Password...">
             <button type="submit" name="submit">Login</button>
 
         </form>
     </div>
+
+    <?php
+        if (isset($_GET["error"])) {
+            if ($_GET["error"] == "emptyinput") {
+                echo "<p>Fill in all fields!</p>";
+            }
+            else if ($_GET["error"] == "invalidemail") {
+                echo "<p>Enter valid email address!</p>";
+            }    
+        }
+    
+    
+    
+    ?>
 </section>
 
 
--- signup.php
+++ signup.php
@@ -21,23 +21,23 @@
         if ($_GET["error"] == "emptyinput") {
             echo "<p>Fill in all fields!</p>";
         }
-        else if ($_get["error"] == "invaliduid") {
+        else if ($_GET["error"] == "invaliduid") {
             echo "<p>Username should contain only alphanumericals!</p>";
         }
-        else if ($_get["error"] == "invalidemail") {
-            echo "<p>Enter valid email address! </p>";
+        else if ($_GET["error"] == "invalidemail") {
+            echo "<p>Enter valid email address!</p>";
         }
-        else if ($_get["error"] == "passwordsdontmatch") {
-            echo "<p>Passwords don't match! </p>";
+        else if ($_GET["error"] == "passwordsdontmatch") {
+            echo "<p>Passwords don't match!</p>";
         }
-        else if ($_get["error"] == "usernametaken") {
-            echo "<p>Username already exists! </p>";
+        else if ($_GET["error"] == "usernametaken") {
+            echo "<p>Username already exists!</p>";
         }
-        else if ($_get["error"] == "stmtfailed") {
-            echo "<p>Something went wrong, try again! </p>";
+        else if ($_GET["error"] == "stmtfailed") {
+            echo "<p>Something went wrong, try again!</p>";
         }
-        else if ($_get["error"] == "none") {
-            echo "<p>You have signed up! </p>";
+        else if ($_GET["error"] == "none") {
+            echo "<p>You have signed up!</p>";
         }   
 
     }
